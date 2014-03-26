1e490228f5dc03743d3468d1a16fd9f05195027e
Marek Lewandowski <marek.m.lewandowski@gmail.com>
2014-03-26 20:27:21 +0100

    Maven project from archetype

---
+++ ontology/src/main/java/emse/abs/hotelmania/ontology/App.java
@@ -0,0 +1,13 @@
+package emse.abs.hotelmania.ontology;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
